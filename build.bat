a64552c3b5ed95d6275986bdc033f3adb978311c
Jack <khevse@gmail.com>
2015-09-12 01:42:23 +0300

    Translation comments

--- build.bat
+++ build.bat
@@ -24,17 +24,17 @@ for /F "tokens=*" %%f in ('go env') DO (
 
 go env
 
-rem работает, но линковка динамическая
+rem work (Dynamic link. it's not good)
 echo v1. build %cd% with CGO_ENABLED=1 go install ...
 SET "CGO_ENABLED=1"
 go install
 
-rem не работает (error: utils\compressor.go:4:2: no buildable Go source files in .....\ConfRobber\src\zlibwrapper)
+rem don't work (error: utils\compressor.go:4:2: no buildable Go source files in .....\ConfRobber\src\zlibwrapper)
 echo v2. build %cd% with CGO_ENABLED=0 go install ...
 SET "CGO_ENABLED=0"
 go install
 
-rem не работает (error: not link libzlibstatic.a)
+rem don't work (error: not link libzlibstatic.a)
 echo v3. build %cd% with CGO_ENABLED=1 go build ...
 SET "CGO_ENABLED=1"
 go build --ldflags "-linkmode internal" -a -v -x
